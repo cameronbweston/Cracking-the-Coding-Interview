e57eadc7e2cae0e9a7890526d1c7ab5a0b5391e8
Cameron Weston <cameron.weston@motominer.com>
2022-03-02 08:55:01 -0600

    Outline all problems for chapter 1

--- ArraysAndLists.cs
+++ ArraysAndLists.cs
@@ -1,7 +1,25 @@
 ﻿
-//1.1 - Naive solution
+//1.1 - IsUnique
+//Naive solution
 bool UniqueChars(string s) {
+
     return true;
 }   
 
 var u = UniqueChars("bla");
+
+//1.2 - Check Permutation
+
+//1.3 - URLify
+
+//1.4 - Palindrome Permutation
+
+//1.5 - One Away
+
+//1.6 - String Compression
+
+//1.7 - Rotate Matrix
+
+//1.8 - Zero Matrix
+
+//1.9 - String Rotation
